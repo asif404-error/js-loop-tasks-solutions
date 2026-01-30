919d5cc4e4c8fbaade1f5545108adac87ad03726
PhantomOblivion <smasif404@gmail.com>
2026-01-30 17:32:24 +0600

    To be continued....

---
+++ while-solutions/odd-even-02.js
@@ -0,0 +1,23 @@
+/*
+Subtask-1: Find all the odd numbers from 61 to 100.
+
+Subtask-2: Find all the even numbers from 78 to 98.
+*/
+
+//Task-01:
+
+let n1 = 61;
+ while(n1<=100){
+    console.log(n1);
+    n1 += 2;
+}
+
+
+//Task-02:
+
+let n2 = 78;
+
+while(n2 <= 98){
+    console.log(n2);
+    n2 += 2;
+}
